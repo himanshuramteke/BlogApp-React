aae8d5a1cc9c155cada76d53d42df1d4b1657366
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-09-21 22:30:15 +0530

    added files in components

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,19 @@ import Footer from "./Footer/Footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "./Signup";
+import AuthLayout from "./AuthLayout";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
 
 export {
     Header, 
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Signup,
+    AuthLayout,
+    RTE,
+    PostForm
 }
